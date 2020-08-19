a9ee08c7b6229141e1d9a7b01242ecbd280e8432
Jonathan Orlen Pellondou <54181323+jonathanorlen@users.noreply.github.com>
2020-08-19 22:32:05 +0700

    migrate html to css

--- src/App.js
+++ src/App.js
@@ -3,9 +3,40 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="container">
+      <div className="frame">
+        <section className="header">
+          <button className="header-btn main-black-color">Add</button>
+          <h1 className="header-title">To DO List</h1>
+          <button className="header-btn main-red-color">Clear</button>
+        </section>
+        <section className="add">
+          <form action="" className="add-form">
+            <input type="text" className="add-input" />
+            <button className="add-btn main-black-color">Add</button>
+          </form>
+        </section>
+        <section className="todos">
+          <div className="todo">
+            <div className="todo-text">Learning React</div>
+          </div>
+          <div className="todo">
+            <div className="todo-text">Learning React</div>
+          </div>
+          <div className="todo">
+            <div className="todo-text">Learning React</div>
+          </div>
+          <div className="todo">
+            <div className="todo-text">Learning React</div>
+          </div>
+          <div className="todo">
+            <div className="todo-text">Learning React</div>
+          </div>
+          <div className="todo">
+            <div className="todo-text">Learning React</div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
